b5a25cfb7550eec24bc0ac819cfbe70c195167d6
Ismael Choque <ismadev2.7@gmail.com>
2020-06-07 20:33:39 -0400

    Updates

---
+++ precache-manifest.f0bd275f79d254b268e92912a438571d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eac8b1e50d56ed7342f3224cad4ccd2",
+    "url": "/social-media-dashboard/index.html"
+  },
+  {
+    "revision": "1115308b94fbfd3ba09b",
+    "url": "/social-media-dashboard/static/css/main.a44c379e.chunk.css"
+  },
+  {
+    "revision": "54a92e2d756afabc1ada",
+    "url": "/social-media-dashboard/static/js/2.5fe32ca7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/social-media-dashboard/static/js/2.5fe32ca7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1115308b94fbfd3ba09b",
+    "url": "/social-media-dashboard/static/js/main.5e42f51b.chunk.js"
+  },
+  {
+    "revision": "61e8bab6cbbd563a937b",
+    "url": "/social-media-dashboard/static/js/runtime-main.4adb50e6.js"
+  }
+]);
